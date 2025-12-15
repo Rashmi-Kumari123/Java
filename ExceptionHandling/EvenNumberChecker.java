543c908480f400095a86c1705d6a5f0f6aa3cf56
Rashmi kumari <143412830+Rashmi-Kumari123@users.noreply.github.com>
2025-12-16 00:38:34 +0530

    oddNo exception handed using throws, thorw method when I am checking for even number

--- ExceptionHandling/EvenNumberChecker.java
+++ ExceptionHandling/EvenNumberChecker.java
@@ -7,9 +7,7 @@ public class EvenNumberChecker {
     }
     public static void main(String[] args) {
         try {
-            
             checkEven(4);
-            
             checkEven(7);
         } catch (OddNumberException e) {
             e.printStackTrace();;
