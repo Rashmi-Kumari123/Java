b9f33656651c79477458f5ac85270f83601398ff
Rashmi kumari <143412830+Rashmi-Kumari123@users.noreply.github.com>
2025-12-15 18:53:50 +0530

    Update Mutable.java

--- mutable/Mutable.java
+++ mutable/Mutable.java
@@ -1,38 +1,30 @@
 //import Immutable;
-
 public class Mutable extends Immutable {
     private int realValue;
     public Mutable(int realValue){
         super(realValue);
         this.realValue = realValue;
-
     }
     public void setRealValue(int v){
         this.realValue = v;
     }
     public int getRealValue(){
         return realValue;
-
-    
     }
     public static void main(String[] args) {
         Mutable obj = new Mutable(4);
         Immutable imObj = (Immutable)obj;  // Upcasting
         System.out.println(imObj.getValue());  // Will print 4
         obj.setRealValue(8);
-        System.out.println(imObj.getValue());  // Will still print 4
+       System.out.println(imObj.getValue());  // Will still print 4
     }
-
-    
 }
 
 class Immutable {
     private final int value;  
-    
     public Immutable(int value) {
-        this.value = value;
+       this.value = value;
     }
-    
     public int getValue() {  
         return value;
     }
